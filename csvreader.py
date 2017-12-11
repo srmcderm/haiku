3742bea50250f5dd1db7959c9c1d8f1fc6931675
srmcderm <fv4105@wayne.edu>
2017-12-11 12:13:18 -0500

    list of loops to pull words based on criteria

--- csvreader.py
+++ csvreader.py
@@ -1,8 +1,30 @@
-#author: @shannonmcdermitt
+# author: @shannonmcdermitt
+
 import csv
 
-# print CSV as list
+# turn CSV into dictionary
 with open('love.csv') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
+    reader = csv.DictReader(f)
+    words = list(reader)
+    
+    # convert syllable #'s into integers
+    for w in words:
+        w['syllables'] = int(w['syllables'])
+        print(w['syllables'])
+    
+    # print only words with 'love' as topic
+    for w in words:
+        if w['topic'] == 'love':
+            print(w['word'])
+    
+    # print only words with 'space' as topic
+    for w in words:
+        if w['topic'] == 'space':
+            print(w['word'])
+    
+    # print words with 2 syllables
+    for w in words:
+        if w['syllables'] == 2:
+            print(w['word'])
+    
+    
